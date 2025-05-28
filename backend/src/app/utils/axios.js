048561dd05ede6e97a2af85b91dcc89f42cb6371
Sakib Ansari <152527120+sakibansari546@users.noreply.github.com>
2025-05-29 03:03:13 +0530

    fix: just remove sulu auth hreader for free sulu

--- backend/src/app/utils/axios.js
+++ backend/src/app/utils/axios.js
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({
   baseURL: `${env.JUDGE0_API_URL}`,
   headers: {
     Accept: "application/json",
-    Authorization: `Bearer ${env.JUDGE0_SULU_KEY}`,
+    // Authorization: `Bearer ${env.JUDGE0_SULU_KEY}`,
   },
   withCredentials: true,
 });
